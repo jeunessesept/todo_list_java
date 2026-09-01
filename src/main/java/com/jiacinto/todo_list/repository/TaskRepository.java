486edecd5c41c5f79819a5e60097ce2811b8a804
jeunessesept <b.jiacinto@gmail.com>
2026-09-01 16:35:27 +0200

    task repo + services : overdue data + comments

--- src/main/java/com/jiacinto/todo_list/repository/TaskRepository.java
+++ src/main/java/com/jiacinto/todo_list/repository/TaskRepository.java
@@ -2,8 +2,14 @@ package com.jiacinto.todo_list.repository;
 
 import com.jiacinto.todo_list.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
 
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     // les méthodes CRUD (find, create, update, delete) sont héritées de JpaRepository
+    
+    // Retourne les tâches dont la date limite (dueDate) est dépassée et qui ne sont pas terminées (completed = false)
+    List<Task> findByDueDateBeforeAndCompletedFalse(LocalDate dueDate);
+
 }
--- src/main/java/com/jiacinto/todo_list/service/TaskService.java
+++ src/main/java/com/jiacinto/todo_list/service/TaskService.java
@@ -5,21 +5,24 @@ import com.jiacinto.todo_list.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class TaskService {
-
+    // reférence le repository utilisé par le service pour accéder aux données des tâches
     private final TaskRepository taskRepository;
 
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
+    // récupère toutes les tâches
     public List<Task> getAllTasks(){
         return taskRepository.findAll();
     }
 
+    //recupère une tâche par son identifiant (id) et retourne une erreur 404 si elle n'existe pas
     public Task getTaskById(Long id){
         return taskRepository.findById(id)
                 .orElseThrow(() -> 
@@ -27,13 +30,20 @@ public class TaskService {
                 );
     }
 
+    // crée et enregistre une nouvelle tâche 
     public Task createTask(Task task){
         return taskRepository.save(task);
     }
 
+    // marque une taâche comme terminée (completed = true)
     public Task completeTask(Long id){
         Task task = getTaskById(id);
         task.setCompleted(true);
         return taskRepository.save(task);
+    }
+
+    // recupère les tâches dont la date limite (dueDate) est dépassée et non terminées
+    public List<Task> getOverdueTasks(){
+        return taskRepository.findByDueDateBeforeAndCompletedFalse(LocalDate.now());
     }
 }
